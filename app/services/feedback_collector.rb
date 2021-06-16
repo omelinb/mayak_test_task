64f3c59ec12f0c2f99afb8b0980556103356d838
omelinb <elros.ob@gmail.com>
2021-06-16 05:10:00 +0300

    Added gems faraday & dotenv; updated feedback service

--- app/services/feedback_collector.rb
+++ app/services/feedback_collector.rb
@@ -1,4 +1,15 @@
 class FeedbackCollector < ApplicationService
+  FEEDBACK_URL = 'https://public-feedbacks.wildberries.ru/api/v1/feedbacks'
+  QUERY_PARAMS = {
+    skip: 0,
+    imtId: ENV['IMTID'].to_i,
+    order: 'dateDesc',
+    take: 10
+  }.to_json
+
   def call
+    response = Faraday.post(FEEDBACK_URL, QUERY_PARAMS)
+    # byebug
+    puts response
   end
 end
