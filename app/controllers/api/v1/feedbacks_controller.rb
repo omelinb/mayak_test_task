b486d7f3e2ac32975c83a7d8bc5c356e0441c838
omelinb <elros.ob@gmail.com>
2021-06-18 06:03:10 +0300

    Little fixes

--- app/controllers/api/v1/feedbacks_controller.rb
+++ app/controllers/api/v1/feedbacks_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::FeedbacksController < ApplicationController
   def feedbacks_activity
     feedbacks = Feedback.where(imt_id: params[:imt_id]).recent(30)
-    render json: feedbacks.group_by_day(:left_at).count
+    grouped_feedbacks = feedbacks.group_by_day(:left_at).count
+    render json: grouped_feedbacks
   end
 
-  def most_commented_in_week
+  def most_commented_this_week
     feedbacks_by_product = Feedback.recent(7).group(:imt_id).count
-    render json: feedbacks_by_product.sort_by { |k, v| v }.last(10).to_h
+    most_commented_products = feedbacks_by_product.sort_by { |k, v| v }.last(10).to_h
+    render json: most_commented_products
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'feedbacks_activity/:imt_id', to: 'feedbacks#feedbacks_activity'
-      get 'most_commented_in_week', to: 'feedbacks#most_commented_in_week'
+      get 'most_commented_this_week', to: 'feedbacks#most_commented_this_week'
     end
   end
 end
